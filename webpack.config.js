ff96938da17269c9fc6776273ea90d56b37de8e9
Arif Sami SAHIN <arifsamisahin@gmail.com>
2021-05-01 22:33:10 +0300

    css loader and post css added

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+
 let mode = "development";
 
 if (process.env.NODE_ENV === "production") {
@@ -10,6 +12,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.s?css$/i,
+        use: [
+          miniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
@@ -19,8 +30,11 @@ module.exports = {
     ],
   },
 
+  plugins: [new miniCssExtractPlugin()],
+
   devtool: "source-map",
   devServer: {
     contentBase: "./dist",
+    hot: true,
   },
 };
